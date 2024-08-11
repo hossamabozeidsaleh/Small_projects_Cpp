67c17ebcf72f7334e68f681bcf6a628b846cbe55
hossam abozeid <hossamabozeidsaleh@gmail.com>
2024-08-10 21:03:22 -0400

    Happened_year.cpp

---
+++ Happened_year.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	
+	int year;
+	cin >> year;
+	switch (year)
+	{
+	case 1982:
+	cout << "My Birth Day";
+	break;
+	case 1983:
+	cout << "My First Work";
+	break;
+	case 1984:
+	cout << "reated My vBulletin Forum";
+	break;
+	case 1985:
+	cout << "Windows Millennium";
+	break;
+	default:
+	cout << "year Is Not Valid";
+	}
+
+
+}
